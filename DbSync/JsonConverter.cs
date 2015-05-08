42c9df233e169b7a02a742256dd94ced2f3cb0f6
mirhagk <mirhagk@gmail.com>
2015-05-08 16:10:49 -0400

    Started on json converter

---
+++ DbSync/JsonConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace DbSync
+{
+    static class JsonConverter
+    {
+        private static string RegexReplace(this string input, string pattern, string replacement)
+        {
+            return Regex.Replace(input, pattern, replacement);
+        }
+        public static string ConvertToJson(string data)
+        {
+            return data
+                    .RegexReplace("<root>", "[")
+                    .RegexReplace("</root>", "]")
+                    .RegexReplace("<row", "{")
+                    .RegexReplace(@"(?<="")\B", ",")
+                    .RegexReplace(", />", "},")
+                    .RegexReplace(@"},\n]", "}\n]")
+                    .RegexReplace(@"\b(?<=\W)(?<=[^""])", "\"")
+                    .RegexReplace(@"\b(?=\W)(?=[^""])", "\"")
+                    .RegexReplace("=", ":");
+        }
+    }
+}
