dba8d54bd92196173561b05ed9f5647686d392af
Nathan Jervis <mirhagk@gmail.com>
2016-01-14 21:31:39 -0500

    Added nuget assembly info

--- DbSync.Core/Properties/AssemblyInfo.cs
+++ DbSync.Core/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("DbSync.Core")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Allows syncronizing a set of xml files to a set of database tables.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Nathan Jervis")]
 [assembly: AssemblyProduct("DbSync.Core")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.*")]
+[assembly: AssemblyFileVersion("0.1.*")]
