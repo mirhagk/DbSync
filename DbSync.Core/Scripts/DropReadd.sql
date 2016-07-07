3c99a3232cbe9770a1d5b556b4c3b3fd26ff18bf
mirhagk <mirhagk@gmail.com>
2016-07-07 11:11:29 -0400

    Fix dropreadd script to use correct template vars and do identity insert

--- DbSync.Core/Scripts/DropReadd.sql
+++ DbSync.Core/Scripts/DropReadd.sql
@@ -1,4 +1,11 @@
 ﻿DELETE FROM @target
 
-INSERT INTO @Target
-SELECT @Source
+IF OBJECTPROPERTY(OBJECT_ID('@target'), 'TableHasIdentity') = 1
+	SET IDENTITY_INSERT @target ON
+
+INSERT INTO @target(@id, @columns)
+SELECT @id, @columns FROM @source
+
+
+IF OBJECTPROPERTY(OBJECT_ID('@target'), 'TableHasIdentity') = 1
+	SET IDENTITY_INSERT @target OFF
