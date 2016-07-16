74eeba1f2529ecb1fb9a77d1b6e65bb3c353587a
Nathan Jervis <mirhagk@gmail.com>
2016-07-16 18:45:59 -0400

    And WHERE condition to update statement

--- DbSync.Core/IDataWriter.cs
+++ DbSync.Core/IDataWriter.cs
@@ -40,7 +40,7 @@ namespace DbSync.Core
 
         public void Update(Dictionary<string, object> entry)
         {
-            RunSql($"UPDATE {table.QualifiedName} SET {string.Join(", ", table.Fields.Where(f => !f.IsPrimaryKey).Select(f => $"{f.Name} = {entry[f.CanonicalName]}"))}");
+            RunSql($"UPDATE {table.QualifiedName} SET {string.Join(", ", table.Fields.Where(f => !f.IsPrimaryKey).Select(f => $"{f.Name} = {Escape(entry[f.CanonicalName])}"))} WHERE {table.PrimaryKey} = {Escape(entry[table.PrimaryKey])}");
         }
         public void Delete(object key)
         {
