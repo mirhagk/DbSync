0f3658ded8ea73b004264834af4ec1a680143681
Nathan Jervis <mirhagk@gmail.com>
2016-07-16 17:39:54 -0400

    Added Smart Transfer transfer class
    Going forward this will be the thing that is used to actually do the work of coordinating and running the job

---
+++ DbSync.Core/Transfers/SmartTransfer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DbSync.Core.Services;
+
+namespace DbSync.Core.Transfers
+{
+    class SmartTransfer : Transfer
+    {
+        public override void Run(JobSettings settings, string environment, IErrorHandler errorHandler)
+        {
+            var target = new IDataReader
+            foreach(var table in settings.Tables)
+            {
+
+            }
+        }
+    }
+}
