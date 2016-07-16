8d58e0be58583a80c65cca356d1c4b59a6778dd0
Nathan Jervis <mirhagk@gmail.com>
2016-07-16 09:14:29 -0400

    Created IDataWriter interface
    This will be used to delegate the actual work of writing the output

---
+++ DbSync.Core/IDataWriter.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbSync.Core
+{
+    interface IDataWriter
+    {
+        void Connect(Table table);
+        void Add(Dictionary<string,object> entry);
+        void Update(Dictionary<string, object> entry);
+    }
+    class SqlSimpleDataWriter : IDataWriter, IDisposable
+    {
+        Table table;
+        SqlConnection connection;
+        public SqlSimpleDataWriter(string connectionString)
+        {
+            connection = new SqlConnection(connectionString);
+            connection.Open();
+        }
+        public void Connect(Table table)
+        {
+            this.table = table;
+        }
+        void RunSql(string sql)
+        {
+            using (var cmd = new SqlCommand(sql, connection))
+                cmd.ExecuteNonQuery();
+        }
+        string Escape(object value)
+        {
+            return $"'{value}'";
+        }
+        public void Add(Dictionary<string, object> entry)
+        {
+            RunSql($"INSERT INTO {table.QualifiedName} ({string.Join(",", table.Fields)}) VALUES ({string.Join(",", table.Fields.Select(f => Escape(entry[f.CanonicalName])))})";
+            throw new NotImplementedException();
+        }
+
+        public void Update(Dictionary<string, object> entry)
+        {
+            //todo: implement
+        }
+        public void Dispose()
+        {
+            connection.Close();
+            connection.Dispose();
+        }
+    }
+}
