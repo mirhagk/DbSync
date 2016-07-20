22089c8ba6ec1d834e2c6b9fca2f61fa559f959a
Nathan Jervis <mirhagk@gmail.com>
2016-07-19 23:42:13 -0400

    Switch exporter to simpler new method

--- DbSync.Core/Transfers/Exporter.cs
+++ DbSync.Core/Transfers/Exporter.cs
@@ -1,4 +1,5 @@
-﻿using DbSync.Core.Services;
+﻿using DbSync.Core.DataWriter;
+using DbSync.Core.Services;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -15,73 +16,26 @@ namespace DbSync.Core.Transfers
     {
         public static Exporter Instance = new Exporter();
         private Exporter() { }
-        void WriteQueryToXmlFile(SqlCommand cmd, string path, JobSettings settings)
-        {
-            using (var reader = cmd.ExecuteReader())
-            {
-
-                var xmlSettings = new XmlWriterSettings
-                {
-                    NewLineOnAttributes = true,
-                    Indent = true,
-                    IndentChars = "  ",
-                    NewLineChars = Environment.NewLine,
-                    OmitXmlDeclaration = true,
-                };
-                var writer = XmlWriter.Create(path, xmlSettings);
-                writer.WriteStartElement("root");
-
-                while (reader.Read())
-                {
-                    writer.WriteStartElement("row");
-                    for (int i = 0; i < reader.FieldCount; i++)
-                    {
-                        var fieldName = reader.GetName(i);
-                        if (settings.IgnoreAuditColumnsOnExport.Value && settings.IsAuditColumn(fieldName))
-                            continue;
-                        if (reader.IsDBNull(i))//for null values just don't output the attribute at all
-                            continue;
-                        writer.WriteAttributeString(reader.GetName(i), reader.GetValue(i).ToString());
-                    }
-                    writer.WriteEndElement();
-                }
-                writer.WriteEndElement();
-                writer.Flush();
-                writer.Close();
-            }
-        }
         public override void Run(JobSettings settings, string environment, IErrorHandler errorHandler)
         {
-            if (!Directory.Exists(settings.Path))
-                Directory.CreateDirectory(settings.Path);
-            using (var conn = new SqlConnection(settings.ConnectionString))
+
+            using (var connection = new SqlConnection(settings.ConnectionString))
             {
-                conn.Open();
+                connection.Open();
+
                 foreach (var table in settings.Tables)
-                {
-                    if (!table.Initialize(conn, settings, errorHandler))
-                        continue;
-
-                    using (var cmd = conn.CreateCommand())
+                    using (var cmd = connection.CreateCommand())
                     {
-                        cmd.CommandText = "SELECT * FROM " + table.QualifiedName;
-                        if (table.IsEnvironmentSpecific)
+                        table.Initialize(connection, settings, errorHandler);
+                        cmd.CommandText = $"SELECT * FROM {table.QualifiedName}";
+                        var diffGenerator = new DiffGenerator();
+                        using (var source = cmd.ExecuteReader())
+                        using (var target = new XmlRecordDataReader(Path.Combine(settings.Path, table.Name + ".xml"), table))
+                        using (var writer = new XmlDataWriter(table, settings))
                         {
-                            cmd.CommandText = $"SELECT {table.PrimaryKey}, {string.Join(", ", table.DataFields)} FROM {table.QualifiedName} WHERE IsEnvironmentSpecific = 0";
-                        }
-                        cmd.CommandText += " ORDER BY " + table.PrimaryKey;
-                        cmd.CommandType = CommandType.Text;
-
-                        WriteQueryToXmlFile(cmd, Path.Combine(settings.Path, table.Name), settings);
-
-                        if (table.IsEnvironmentSpecific)
-                        {
-                            //Switch IsEnvironmentSpecific = 0 to IsEnvironmentSpecific = 1
-                            cmd.CommandText = cmd.CommandText.Replace("IsEnvironmentSpecific = 0", "IsEnvironmentSpecific = 1");
-                            WriteQueryToXmlFile(cmd, Path.Combine(settings.Path, table.Name) + "." + environment, settings);
+                            diffGenerator.GenerateDifference(source, target, table, writer);
                         }
                     }
-                }
             }
         }
     }
