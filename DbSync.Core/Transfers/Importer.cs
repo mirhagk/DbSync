430ef3ecf5bda4516233211802ee584fb6850e40
Nathan Jervis <mirhagk@gmail.com>
2016-07-23 14:42:26 -0400

    Switch importer to new importer

--- DbSync.Core/Transfers/Importer.cs
+++ DbSync.Core/Transfers/Importer.cs
@@ -17,53 +17,25 @@ namespace DbSync.Core.Transfers
     {
         public static Importer Instance = new Importer();
         private Importer() { }
-		void ImportTable(SqlConnection connection, Table table, JobSettings settings, IErrorHandler errorHandler)
-		{
-			Console.WriteLine($"Importing table {table.Name}");
-
-            connection.Execute(GetTempTableScript(table));
-
-            var mergeStrategy = table.MergeStrategy ?? settings.MergeStrategy;
-
-            table.UseDefaults = mergeStrategy != Merge.Strategy.Override; 
-
-            if (table.ByEnvironment)
-            {
-                if (File.Exists(table.EnvironmentSpecificFileName))
-                    CopyFromFileToTempTable(connection, table.EnvironmentSpecificFileName, table, errorHandler);
-            }
-            else
-                CopyFromFileToTempTable(connection, Path.Combine(settings.Path, table.Name), table, errorHandler);
-            
-			if (table.IsEnvironmentSpecific)
-				if (File.Exists(table.EnvironmentSpecificFileName))
-					CopyFromFileToTempTable(connection, table.EnvironmentSpecificFileName, table, errorHandler);
-
-            try
-            {
-                connection.Execute(Merge.GetSqlForMergeStrategy(settings, table));
-            }
-            catch (SqlException ex)
-            {
-                errorHandler.Error($"Error while importing {table.Name}: {ex.Message}");
-            }
-		}
         public override void Run(JobSettings settings, string environment, IErrorHandler errorHandler)
         {
-            using (var conn = new SqlConnection(settings.ConnectionString))
+            using (var connection = new SqlConnection(settings.ConnectionString))
             {
-                conn.Open();
-                foreach(var table in settings.Tables)
-                    conn.Execute($"ALTER TABLE {table.QualifiedName} NOCHECK CONSTRAINT ALL");
+                connection.Open();
 
                 foreach (var table in settings.Tables)
-                {
-                    if (table.Initialize(conn, settings, errorHandler))
-                        ImportTable(conn, table, settings, errorHandler);
-                }
-
-                foreach (var table in settings.Tables)
-                    conn.Execute($"ALTER TABLE {table.QualifiedName} CHECK CONSTRAINT ALL");
+                    using (var cmd = connection.CreateCommand())
+                    {
+                        table.Initialize(connection, settings, errorHandler);
+                        cmd.CommandText = $"SELECT * FROM {table.QualifiedName}";
+                        var diffGenerator = new DiffGenerator();
+                        using (var target = cmd.ExecuteReader())
+                        using (var source = new XmlRecordDataReader(Path.Combine(settings.Path, table.Name + ".xml"), table))
+                        using (var writer = new SqlSimpleDataWriter(settings.ConnectionString, table, settings))
+                        {
+                            diffGenerator.GenerateDifference(source, target, table, writer);
+                        }
+                    }
             }
         }
     }
