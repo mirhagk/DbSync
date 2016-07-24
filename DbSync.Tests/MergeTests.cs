83836909f3759f6871bbab287901870485a0552c
Nathan Jervis <mirhagk@gmail.com>
2016-07-24 15:41:47 -0400

    Create test for checking if stuff is escaped

--- DbSync.Tests/MergeTests.cs
+++ DbSync.Tests/MergeTests.cs
@@ -3,6 +3,8 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using DbSync.Core;
 using DbSync.Tests.Helpers;
 using System.Xml.Serialization;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace DbSync.Tests
 {
@@ -27,6 +29,11 @@ namespace DbSync.Tests
                 return id.GetHashCode() ^ value.GetHashCode();
             }
         }
+        List<Values> GetValueList(params string[] values)
+        {
+            int id = 1;
+            return values.Select(v => new Tests.MergeTests.Values { id = id++, value = v }).ToList();
+        }
         [TestMethod]
         public void TestSimpleImport()
         {
@@ -35,39 +42,21 @@ namespace DbSync.Tests
                 test.Create();
 
                 test.Initialize();
-                test.Load(new System.Collections.Generic.List<Values> { new Values { id = 1, value = "test" }, new Values { id = 2, value = "test2" } });
+                test.Load(GetValueList("test","test2"));
                 test.RoundTripCheck();
             }
         }
         [TestMethod]
-        public void DoesGetSqlForMergeStrategyRun()
+        public void DoesEscaping()
         {
-            const string connectionString = @"Data Source=.;Database=tempdb;Integrated Security=True";
-
-            var settings = new JobSettings();
-            settings.ConnectionString = connectionString;
-            settings.Path = @"Data";
-            settings.UseAuditColumnsOnImport = false;
-            settings.IgnoreAuditColumnsOnExport = false;
-            settings.MergeStrategy = Merge.Strategy.MergeWithDelete;
-            settings.AuditColumns = new JobSettings.AuditSettings();
-            settings.Tables.Add(new Table()
+            using (var test = new DatabaseTest<Values>())
             {
-                Name = "dbo.SmallTable",
-            });
-            Table table = new Table();
-            table.PrimaryKey = "testID";
-            table.Name = "testTable";
-            //table.DataFields = new[] { "testColumn1", "testColumn2", "testColumn3" }
-            table.MergeStrategy = null;
+                test.Create();
 
-            var sql = Merge.GetSqlForMergeStrategy(settings, table);
-            Assert.IsTrue(sql.Contains("testTable"));
-            Assert.IsTrue(sql.Contains("##testTable"));
-            Assert.IsTrue(sql.Contains("testID"));
-            Assert.IsTrue(sql.Contains("testColumn1"));
-            Assert.IsTrue(sql.Contains("testColumn1"));
-            Assert.IsTrue(sql.Contains("DELETE"));
+                test.Initialize();
+                test.Load(GetValueList("dawdaw\\dwadawd", "dwadaw'dwadaw", "\"\"dawdawda\"", "[]|_13155616378899!@#%$^^^(!#@!)@#)!@#!()~?/>;--\\"));
+                test.RoundTripCheck();
+            }
         }
     }
 }
